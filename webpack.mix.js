de377e47a75c7e5a1ecef29287748b69340ebed7
maks-sl <maxislyshenkov@gmail.com>
2019-09-25 19:34:25 -0700

    Configured laravel-mix, include it in layout

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,9 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+mix
+    .setPublicPath('public/hot')
+    .setResourceRoot('/hot/')
+    .js('resources/assets/js/app.js', 'js')
+    .sass('resources/assets/sass/app.scss', 'css')
+    .version();
