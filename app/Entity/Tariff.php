1b71b0000d2dc42951f10c8e01e41fbd41b55578
maks-sl <maxislyshenkov@gmail.com>
2019-09-27 19:45:42 -0700

    Added custom validator "checkDateForTariff:tariffAttrName"

--- app/Entity/Tariff.php
+++ app/Entity/Tariff.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use LogicException;
 
 /**
  * @property int $id
@@ -22,15 +23,38 @@ class Tariff extends Model
         'allowed_days' => 'array',
     ];
 
-//    public function datesList(): array
-//    {
-//        $now = Carbon::now();
-//        $now->addDays();
-//        $currentDay = $now->dayOfWeek;
-//        if (in_array($currentDay, $this->allowed_days)) {
-//            $firstDayDiff = 0;
-//        } else {
-//
-//        }
-//    }
+    public function allowedDates(): array
+    {
+        if (!empty(array_diff($this->allowed_days, array_keys(Carbon::getDays()))) or empty($this->allowed_days)) {
+            throw new LogicException('Invalid tariff allowed_date');
+        }
+        $now = Carbon::now();
+        $allowedDays = array_sort($this->allowed_days);
+        $nowDay = $now->dayOfWeek;
+        $dates = [];
+
+        if (in_array($nowDay, $this->allowed_days)) {
+            $dates[] = $now->toDateString();
+        }
+        foreach ($allowedDays as $i => $d) {
+            if ($d > $nowDay) {
+                $seek = $i;
+                break;
+            }
+        }
+        if (!isset($seek)) { $seek = 0; }
+
+        $current = clone $now;
+        $limitDate = (clone $now)->addMonth();
+        while (count($dates) < 31) {
+            if ($seek >= count($allowedDays)) { $seek = 0; }
+            $current->next($allowedDays[$seek]);
+            if ($current->greaterThanOrEqualTo($limitDate)) {
+                break;
+            }
+            $dates[] = $current->toDateString();
+            $seek++;
+        }
+        return $dates;
+    }
 }
---
+++ app/Providers/ValidationServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Providers;
+
+use App\Entity\Tariff;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Validator;
+
+class ValidationServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        Validator::extend('checkDateForTariff', function($attribute, $value, $parameters, $validator) {
+            $tariffId = array_get($validator->getData(), $parameters[0], null);
+            if (!empty($tariffId)) {
+                try {
+                    $tariff = Tariff::findOrFail($tariffId);
+                    return $validator->validateIn($attribute, $value, $tariff->allowedDates());
+                } catch (ModelNotFoundException $e) {
+                    return false;
+                }
+            }
+            return false;
+        }, 'Date incorrect for this tariff');
+    }
+
+    public function register()
+    {
+        //
+    }
+}
