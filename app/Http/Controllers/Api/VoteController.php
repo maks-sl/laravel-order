67435a866de85f524da891bd3bd030a7d8456d06
maks-sl <maxislyshenkov@gmail.com>
2019-12-21 13:41:48 -0800

    Fixed raw sql query (added group by field)

--- app/Http/Controllers/Api/VoteController.php
+++ app/Http/Controllers/Api/VoteController.php
@@ -9,7 +9,7 @@ class VoteController extends Controller
 {
     public function index()
     {
-        $results = DB::select( DB::raw('select c.name as label, c.color as backgroundColor, count(v.country_id) as data from votes v join countries c on v.country_id = c.id group by c.name, c.color order by v.country_id') );
+        $results = DB::select( DB::raw('select c.name as label, c.color as backgroundColor, count(v.country_id) as data from votes v join countries c on v.country_id = c.id group by c.name, c.color, v.country_id order by v.country_id') );
         $datasets = array_map(function ($item) {
             return [
                 'label' => $item->label,
