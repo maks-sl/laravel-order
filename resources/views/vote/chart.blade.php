29aa503fededed633db0693df0706857b2c547b4
maks-sl <maxislyshenkov@gmail.com>
2019-12-21 16:58:59 -0800

    Config fetchAddress for Chart via props

--- resources/views/vote/chart.blade.php
+++ resources/views/vote/chart.blade.php
@@ -5,7 +5,7 @@
         <div class="col-12">
             <div class="card">
                 <div class="card-body">
-                    <chart></chart>
+                    <chart fetch-address="/api/results"></chart>
                 </div>
             </div>
         </div>
