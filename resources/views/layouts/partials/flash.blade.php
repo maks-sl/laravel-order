f361ee48dbcd480f4b37ba7e247b0a0254fbfc99
maks-sl <maxislyshenkov@gmail.com>
2019-12-21 18:04:08 -0800

    Edited flash messages template

--- resources/views/layouts/partials/flash.blade.php
+++ resources/views/layouts/partials/flash.blade.php
@@ -1,23 +1,39 @@
 @if (session('status'))
-    <div class="alert alert-success">
-        {{ session('status') }}
+    <div  class="card">
+        <div class="card-body">
+            <div class="alert alert-success">
+                {{ session('status') }}
+            </div>
+        </div>
     </div>
 @endif
 
 @if (session('success'))
-    <div class="alert alert-success">
-        {{ session('success') }}
+    <div  class="card">
+        <div class="card-body">
+            <div class="alert alert-success">
+                {{ session('success') }}
+            </div>
+        </div>
     </div>
 @endif
 
 @if (session('error'))
-    <div class="alert alert-danger">
-        {{ session('error') }}
+    <div  class="card">
+        <div class="card-body">
+            <div class="alert alert-danger">
+                {{ session('error') }}
+            </div>
+        </div>
     </div>
 @endif
 
 @if (session('info'))
-    <div class="alert alert-info">
-        {{ session('info') }}
+    <div  class="card">
+        <div class="card-body">
+            <div class="alert alert-info">
+                {{ session('info') }}
+            </div>
+        </div>
     </div>
 @endif
