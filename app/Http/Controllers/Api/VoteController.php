538f8a5647d1d3b053f8c73c84621b38489937ee
maks-sl <maxislyshenkov@gmail.com>
2019-12-21 16:02:38 -0800

    Fixed API VoteController response format

--- app/Http/Controllers/Api/VoteController.php
+++ app/Http/Controllers/Api/VoteController.php
@@ -11,18 +11,24 @@ class VoteController extends Controller
     public function index()
     {
         $results = DB::select( DB::raw('select c.name as label, c.color as backgroundColor, count(v.country_id) as data from votes v join countries c on v.country_id = c.id group by c.name, c.color, v.country_id order by v.country_id') );
+        // (FLOOR(1 + RAND() * 300))
         $num_results = Vote::all()->count();
-        $datasets = array_map(function ($item) {
-            return [
-                'label' => $item->label,
-                'backgroundColor' => $item->backgroundColor,
-                'data' => [$item->data],
-            ];
-        }, $results);
+        $datasets = [
+            'label' => 'Всего голосов: '. $num_results,
+            'backgroundColor' => [],
+            'data' => [],
+
+        ];
+        $labels = [];
+        foreach ($results as $item) {
+            $datasets['backgroundColor'][] = $item->backgroundColor;
+            $datasets['data'][] = $item->data;
+            $labels[] = $item->label;
+        }
 
         return response()->json([
-            'labels' => ['Всего голосов: '. $num_results],
-            'datasets' => $datasets,
+            'labels' => $labels,
+            'datasets' => [$datasets],
         ]);
     }
 }
