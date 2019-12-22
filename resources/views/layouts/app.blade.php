e1bfaa1e6477ecd09ea4e7e35acf06cdad3a1a1f
maks-sl <maxislyshenkov@gmail.com>
2019-12-21 18:46:54 -0800

    Added pages titles

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,8 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    @section('title', config('app.name', 'Laravel'))
+    <title>@yield('title')</title>
 
     <!-- Styles -->
     <link href="{{ mix('css/app.css', 'hot') }}" rel="stylesheet">
