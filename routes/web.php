7114b0c2304b5c675617eac05d9a35515181a981
maks-sl <maxislyshenkov@gmail.com>
2020-05-16 01:52:48 +0300

    Added auth

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,13 @@
 |
 */
 
+Auth::routes();
+Route::get('/', 'HomeController@index')->name('home');
+
+// ORDER SAMPLE
 //Route::name('welcome')->get('/', function () {
-//    return view('welcome');
+//    return view('order.welcome');
 //});
-//
 //Route::resource('order', 'OrderController')->only('create', 'store');
 //
 //Route::group([
@@ -25,12 +28,13 @@
 //    Route::post('/store', 'OrderController@apiStore')->name('store');
 //});
 
-Route::get('/', 'VoteController@create')->name('vote.create');
-Route::post('/vote/store', 'VoteController@store')->name('vote.store');
-Route::get('/chart', 'VoteController@chart')->name('vote.chart');
-Route::get('/unique', 'VoteController@unique')->name('vote.unique');
-Route::get('/manage-secret-panel', 'PollController@index')->name('poll.index');
-Route::post('/poll/manage', 'PollController@manage')->name('poll.manage');
-Route::get('/time', 'TimeController@index')->name('time.index');
+// VOTE SAMPLE
+//Route::get('/', 'VoteController@create')->name('vote.create');
+//Route::post('/vote/store', 'VoteController@store')->name('vote.store');
+//Route::get('/chart', 'VoteController@chart')->name('vote.chart');
+//Route::get('/unique', 'VoteController@unique')->name('vote.unique');
+//Route::get('/manage-secret-panel', 'PollController@index')->name('poll.index');
+//Route::post('/poll/manage', 'PollController@manage')->name('poll.manage');
+//Route::get('/time', 'TimeController@index')->name('time.index');
 
 //Route::resource('vote', 'VoteController')->only('create', 'store');
