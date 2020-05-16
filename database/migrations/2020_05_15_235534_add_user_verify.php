d1dc53a5cd9dfd7397932c81b143fb4cea051e49
maks-sl <maxislyshenkov@gmail.com>
2020-05-16 03:32:52 +0300

    Added User verify

---
+++ database/migrations/2020_05_15_235534_add_user_verify.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUserVerify extends Migration
+{
+
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('status', 16);
+            $table->string('verify_token')->nullable()->unique();
+        });
+
+        DB::table('users')->update([
+            'status' => 'active',
+        ]);
+    }
+
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('status');
+            $table->dropColumn('verify_token');
+        });
+    }
+}
