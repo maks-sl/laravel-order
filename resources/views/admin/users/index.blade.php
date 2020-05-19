62ee399a5a065221d2ded1bf007be025f0c0988c
maks-sl <maxislyshenkov@gmail.com>
2020-05-19 15:43:02 +0300

    Fixed admin.users views

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -61,43 +61,45 @@
         </div>
     </div>
 
-    <table class="table table-bordered table-striped">
-        <thead>
-        <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Status</th>
-            <th>Role</th>
-        </tr>
-        </thead>
-        <tbody>
-
-        @foreach ($users as $user)
+    <div class="card mb-3 p-4">
+        <table class="table table-bordered table-striped">
+            <thead>
             <tr>
-                <td>{{ $user->id }}</td>
-                <td><a href="{{ route('admin.users.show', $user) }}">{{ $user->name }}</a></td>
-                <td>{{ $user->email }}</td>
-                <td>
-                    @if ($user->isWait())
-                        <span class="badge badge-secondary">Waiting</span>
-                    @endif
-                    @if ($user->isActive())
-                        <span class="badge badge-primary">Active</span>
-                    @endif
-                </td>
-                <td>
-                    @if ($user->isAdmin())
-                        <span class="badge badge-danger">Admin</span>
-                    @else
-                        <span class="badge badge-secondary">User</span>
-                    @endif
-                </td>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Status</th>
+                <th>Role</th>
             </tr>
-        @endforeach
+            </thead>
+            <tbody>
 
-        </tbody>
-    </table>
+            @foreach ($users as $user)
+                <tr>
+                    <td>{{ $user->id }}</td>
+                    <td><a href="{{ route('admin.users.show', $user) }}">{{ $user->name }}</a></td>
+                    <td>{{ $user->email }}</td>
+                    <td>
+                        @if ($user->isWait())
+                            <span class="badge badge-secondary">Waiting</span>
+                        @endif
+                        @if ($user->isActive())
+                            <span class="badge badge-primary">Active</span>
+                        @endif
+                    </td>
+                    <td>
+                        @if ($user->isAdmin())
+                            <span class="badge badge-danger">Admin</span>
+                        @else
+                            <span class="badge badge-secondary">User</span>
+                        @endif
+                    </td>
+                </tr>
+            @endforeach
+
+            </tbody>
+        </table>
+    </div>
 
     {{ $users->links() }}
 @endsection
--- resources/views/admin/users/show.blade.php
+++ resources/views/admin/users/show.blade.php
@@ -20,39 +20,41 @@
         </form>
     </div>
 
-    <table class="table table-bordered table-striped">
-        <tbody>
-        <tr>
-            <th>ID</th><td>{{ $user->id }}</td>
-        </tr>
-        <tr>
-            <th>Name</th><td>{{ $user->name }}</td>
-        </tr>
-        <tr>
-            <th>Email</th><td>{{ $user->email }}</td>
-        </tr>
-        <tr>
-            <th>Status</th>
-            <td>
-                @if ($user->isWait())
-                    <span class="badge badge-secondary">Waiting</span>
-                @endif
-                @if ($user->isActive())
-                    <span class="badge badge-primary">Active</span>
-                @endif
-            </td>
-        </tr>
-        <tr>
-            <th>Role</th>
-            <td>
-                @if ($user->isAdmin())
-                    <span class="badge badge-danger">Admin</span>
-                @else
-                    <span class="badge badge-secondary">User</span>
-                @endif
-            </td>
-        </tr>
-        <tbody>
-        </tbody>
-    </table>
+    <div class="card mb-3 p-4">
+        <table class="table table-bordered table-striped">
+            <tbody>
+            <tr>
+                <th>ID</th><td>{{ $user->id }}</td>
+            </tr>
+            <tr>
+                <th>Name</th><td>{{ $user->name }}</td>
+            </tr>
+            <tr>
+                <th>Email</th><td>{{ $user->email }}</td>
+            </tr>
+            <tr>
+                <th>Status</th>
+                <td>
+                    @if ($user->isWait())
+                        <span class="badge badge-secondary">Waiting</span>
+                    @endif
+                    @if ($user->isActive())
+                        <span class="badge badge-primary">Active</span>
+                    @endif
+                </td>
+            </tr>
+            <tr>
+                <th>Role</th>
+                <td>
+                    @if ($user->isAdmin())
+                        <span class="badge badge-danger">Admin</span>
+                    @else
+                        <span class="badge badge-secondary">User</span>
+                    @endif
+                </td>
+            </tr>
+            <tbody>
+            </tbody>
+        </table>
+    </div>
 @endsection
